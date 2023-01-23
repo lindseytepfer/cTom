e4c90ada99208790b275cbada5728482c804892d
LT <lindsey.j.tepfer.gr@dartmouth.edu>
2023-01-23 16:15:00 -0500

    more styling

---
+++ components/components/experiment.js
@@ -0,0 +1,181 @@
+import React, { useState } from 'react';
+//import data from '/Users/f004p74/Desktop/web-dev/c-tom-app/src/ctom-data_0.json';
+import { Ratings } from './ratings.js';
+import { Grid, Typography, Button } from "@mui/material";
+
+const data = {'Block1': {'target10': {'face': {'Bossy': 0, 'Easygoing': 0, 'Passive': 0},
+   'video': {'Passive': 0, 'Bossy': 0, 'Easygoing': 0},
+   'convo': {'Easygoing': 0, 'Bossy': 0, 'Passive': 0},
+   'self': {'Rationality': 0, 'Impact': 0, 'Valence': 0},
+   'partnerState': {'Rationality': 0, 'Impact': 0, 'Valence': 0},
+   'partnerTrait': {'Passive': 0, 'Easygoing': 0, 'Bossy': 0}}},
+ 'Block2': {'target13': {'face': {'Bossy': 0, 'Easygoing': 0, 'Passive': 0},
+   'video': {'Bossy': 0, 'Passive': 0, 'Easygoing': 0},
+   'convo': {'Easygoing': 0, 'Passive': 0, 'Bossy': 0},
+   'self': {'Rationality': 0, 'Impact': 0, 'Valence': 0},
+   'partnerState': {'Impact': 0, 'Rationality': 0, 'Valence': 0},
+   'partnerTrait': {'Passive': 0, 'Bossy': 0, 'Easygoing': 0}}}}
+
+const blockList = [];
+const targetList = [];
+const stimList = ["face","video","convo","self","partnerState","partnerTrait"];
+const traitList = [];
+
+for (let block in data) {
+  let r = [];
+  blockList.push(block)
+  for (let target in data[block]) {
+    targetList.push(target)
+    for (let stimuli in data[block][target]){
+      for (let rating in data[block][target][stimuli]){
+        r.push(rating)
+      }
+    }
+  }
+  traitList.push(r)
+}
+
+export const Experiment = ({ pageEvent }) => {
+  const [blockState,setBlockState] = useState(0);
+  const [targetState,setTargetState] = useState(0); 
+  const [stimState, setStimState] = useState(0);
+  const [traitState,setTraitState] = useState(0);
+  const [rating1, setRating1] = useState(0)
+  const [rating2, setRating2] = useState(0)
+  const [rating3, setRating3] = useState(0)
+
+  const handleChange1 = (e) => {
+    setRating1(e.target.value);
+  }
+  const handleChange2 = (e) => {
+    setRating2(e.target.value);
+  }
+  const handleChange3 = (e) => {
+    setRating3(e.target.value);
+  }
+
+  const saveData = () => {
+    data[blockList[blockState]][targetList[targetState]][stimList[stimState]][traitList[blockState][traitState]] = rating1;
+    data[blockList[blockState]][targetList[targetState]][stimList[stimState]][traitList[blockState][traitState+1]] = rating2;
+    data[blockList[blockState]][targetList[targetState]][stimList[stimState]][traitList[blockState][traitState+2]] = rating3;
+    setRating1(0);
+    setRating2(0);
+    setRating3(0);
+  }
+  
+  const nextTrial = () => {
+    saveData();
+    setStimState((prev) => prev + 1);
+    setTraitState((prev) => prev + 3);
+  }
+  
+  const advBlock = () => {
+    setBlockState((prev) => prev + 1);
+    setTargetState((prev) => prev + 1);
+    setStimState(0);
+    setTraitState(0);
+  }
+
+  return (
+    <>
+      {stimList[stimState] === "face" && 
+        <>
+          <Grid container justifyContent="center" paddingTop={10}>
+          <Typography style={{color: "#353834"}} align="center">
+            <img src={`stim/${targetList[targetState]}/face.png`} alt="face" />
+            <p>How would you rate this person on the following traits?</p>
+          </Typography>
+          </Grid>
+          <Ratings traitlist={traitList} blockstate={blockState} traitstate={traitState} change1={handleChange1} change2={handleChange2} change3={handleChange3} advStim={nextTrial} />
+        </>
+      }
+
+      {stimList[stimState] === "video" && 
+        <>
+          <Grid container justifyContent="center" paddingTop={10}>
+          <Typography style={{color: "#353834"}} align="center">
+            <video src={`stim/${targetList[targetState]}/video.mp4`}>Unable to load video.</video>
+            <p>After watching the video, now how would you rate this person on the following traits? </p>
+          </Typography>
+          </Grid>
+          <Ratings traitlist={traitList} blockstate={blockState} traitstate={traitState} change1={handleChange1} change2={handleChange2} change3={handleChange3} advStim={nextTrial} />
+        </>
+      }
+
+      {stimList[stimState] === "convo" && 
+        <>
+          <Grid container justifyContent="center" paddingTop={10}>
+          <Typography style={{color: "#353834"}} align="center">
+            <img src={`stim/${targetList[targetState]}/face.png`} alt="face" />
+            <p>After discussing this person with your partner, how would you rate this person on the following traits?</p>
+          </Typography>
+          </Grid>
+          <Ratings traitlist={traitList} blockstate={blockState} traitstate={traitState} change1={handleChange1} change2={handleChange2} change3={handleChange3} advStim={nextTrial} />
+        </>
+      }
+
+      {stimList[stimState] === "self" && 
+        <>
+          <Grid container justifyContent="center" paddingTop={10}>
+          <Typography style={{color: "#353834"}} align="center">
+            <p>How would you rate your current experience using these three states?</p>
+          </Typography>
+          </Grid>
+          <Ratings traitlist={traitList} blockstate={blockState} traitstate={traitState} change1={handleChange1} change2={handleChange2} change3={handleChange3} advStim={nextTrial} />
+          
+        </>
+      }
+
+      {stimList[stimState] === "partnerState" && 
+        <>
+          <Grid container justifyContent="center" paddingTop={10}>
+          <Typography style={{color: "#353834"}} align="center">
+            <p>At this point, which of these three states do you think your partner is currently experiencing? </p>
+          </Typography>
+          </Grid>
+          <Ratings traitlist={traitList} blockstate={blockState} traitstate={traitState} change1={handleChange1} change2={handleChange2} change3={handleChange3} advStim={nextTrial} />
+        </>
+      }
+
+      {stimList[stimState] === "partnerTrait"&& 
+        <>
+          <Grid container justifyContent="center" paddingTop={10}>
+          <Typography style={{color: "#353834"}} align="center">
+            <p>How would you rate your partner on the following traits?</p>
+          </Typography>
+          </Grid>
+          <Ratings traitlist={traitList} blockstate={blockState} traitstate={traitState} change1={handleChange1} change2={handleChange2} change3={handleChange3} advStim={nextTrial} />
+        </>
+      }
+
+      {traitState === 18 && 
+        <>
+          <Grid container justifyContent="center" paddingTop={10}>
+          <Typography style={{color: "#353834"}} align="center">
+            <p>When you're ready, click the button below to begin the next trial.</p>
+
+          <Button style={{color: "#FFFFFF",
+                            fontSize: "15px",
+                            backgroundColor: "#006633"
+            }} onClick={advBlock}> Start Next Trial </Button>
+          </Typography>
+          </Grid>
+        </>
+      }
+
+      {blockState === 15 && 
+        <>
+          <Grid container justifyContent="center">
+          <Typography style={{color: "#353834"}} align="center">
+            <p>You have completed the study.</p>
+            <Button style={{color: "#FFFFFF",
+                              fontSize: "15px",
+                              backgroundColor: "#006633"
+              }} onClick={pageEvent}> Debrief</Button>
+          </Typography>
+          </Grid>
+        </>
+      }
+    </>
+    )
+}
---
+++ components/components/ratings.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import { Grid, Typography, Button, Slider } from "@mui/material";
+
+export const Ratings = ( props ) => {
+    const [slider1Moved, setSlider1Moved] = useState("false")
+    const [slider2Moved, setSlider2Moved] = useState("false")
+    const [slider3Moved, setSlider3Moved] = useState("false")
+
+    return (
+        <>
+            <Grid container justifyContent="center">
+            <Typography style={{color: "rgb(33,37,40)"}} align="center">
+                <form>
+                    <label>
+                    <p>{props.traitlist[props.blockstate][props.traitstate]}</p>
+                    <Slider valueLabelDisplay="auto" color="secondary" min={0} max={100} onChange={props.change1} onClick={()=>setSlider1Moved("true")} />
+                    </label>
+    
+                    <label>
+                    <p>{props.traitlist[props.blockstate][props.traitstate+1]}</p>
+                    <Slider valueLabelDisplay="auto" color="secondary" min={0} max={100} onChange={props.change2} onClick={()=>setSlider2Moved("true")} />
+                    </label>
+        
+                    <label>
+                    <p>{props.traitlist[props.blockstate][props.traitstate+2]}</p>
+                    <Slider valueLabelDisplay="auto" color="secondary" min={0} max={100} onChange={props.change3} onClick={()=>setSlider3Moved("true")} />
+                    </label>
+                </form>
+                <br />
+                {slider1Moved === "true" & slider2Moved === "true" & slider3Moved === "true" ? 
+                        <Button style={{
+                            color: "#FFFFFF",
+                            fontSize: "15px",
+                            backgroundColor: "#006633",
+                        }} onClick={props.advStim}> Next</Button> : <p></p>
+                }
+            </Typography>
+            </Grid>
+        </>
+    )
+}
