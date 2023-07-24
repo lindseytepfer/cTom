722ee4e53550110a1c5cda21e0cfeb5145e5dcd1
LT <lindsey.j.tepfer.gr@dartmouth.edu>
2023-07-24 14:17:34 -0400

    Revised app. js file (no debrief)

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,31 @@
-import './App.css';
-import React, { useState } from 'react';
-import { Intro } from './components/intro';
-import { Experiment } from './components/experiment';
-import { Debrief } from './components/debrief';
+import React, { useState } from "react";
+import { Intro } from "./components/intro";
+import { Experiment } from "./components/experiment";
+//import { Experiment } from "./components/expTester";
+import io from "socket.io-client";
+
+const socket = io.connect("http://localhost:3001/");
 
 const App = () => {
   const [pageState, setPageState] = useState(0);
+  const [pairID, setPairID] = useState(""); 
+  const [subjectID, setSubjectID] = useState([]);
   const nextPage = () => setPageState((prev) => prev + 1);
 
-  return (
-      <div className="App">
-        {(() => {
-          switch (pageState) {
-            case 0:
-              return <Intro pageEvent={nextPage} />
-            case 1:
-              return <Experiment pageEvent={nextPage} />
-            case 2:
-              return <Debrief />
-            default:
-              return null
-          }
-        })()}
-      </div>
-
-
-  );
-}
+    return (
+        <div className="App">
+            {(() => {
+                switch (pageState) {
+                  case 0:
+                    return <Intro pageEvent={nextPage} socket={socket} pairID={pairID} setPairID={setPairID} subjectID={subjectID} setSubjectID={setSubjectID} />;
+                    case 1:
+                        return <Experiment socket={socket} subjectID={subjectID} pairID={pairID} />;
+                    default:
+                        return null;
+                }
+            })()}
+        </div>
+    );
+};
 
 export default App;
