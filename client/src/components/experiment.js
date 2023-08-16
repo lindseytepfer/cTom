cafb6963e6337548c9a462b9ef2abee64bb8bc3e
LT <lindsey.j.tepfer.gr@dartmouth.edu>
2023-08-16 16:55:05 -0400

    Adjusted trial sequence timing [WIP]
    Still debugging

--- client/src/components/experiment.js
+++ client/src/components/experiment.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { TraitRatings } from './traitRatings.js';
 import { StateRatings } from './stateRatings.js';
 import { CountdownTimer } from './countdownTimer.js';
@@ -6,6 +6,9 @@ import { Grid, Typography, Button, lighten } from "@mui/material";
 import axios from "axios";
 
 //import data from "/Users/f004p74/Desktop/web-dev/c-tom-app/src/ctom-data_0001.json"
+const stimList = ["ITI","faceTrait", "faceState", "videoTrial","videoTrait", "videoState","predictTrait","predictState",
+  "convoTrial","convoTrait","convoState","selfState","partnerTrait","partnerState","endTrial"];
+//videoTrial = stimList[2]; convoTrial = stimList[7]
 
 const data = {'Block1': {'target10': {'faceTrait': {'Bossy': 0,
 'Easygoing': 0,
@@ -34,24 +37,11 @@ const data = {'Block1': {'target10': {'faceTrait': {'Bossy': 0,
 'partnerTrait': {'Easygoing': 0, 'Passive': 0, 'Bossy': 0},
 'partnerState': {'Impact': 0, 'Valence': 0, 'Rationality': 0}}}}
 
-const videoDurations = {'target1':115000,
-  'target2':161000,
-  'target3':173000,
-  'target4':80000,
-  'target5':153000,
-  'target6':163000,
-  'target7':120000,
-  'target8':176000,
-  'target9':103000,
-  'target10':69000,
-  'target11':167000,
-  'target12':92000,
-  'target13':163000,
-  'target14':126000}
+const videoDurations = {'target1':115000,'target2':161000,'target3':173000,'target4':80000,'target5':153000,'target6':163000,'target7':120000,
+  'target8':176000,'target9':103000,'target10':69000,'target11':167000,'target12':92000,'target13':163000,'target14':126000}
 
 const blockList = [];
 const targetList = [];
-const stimList = ["faceTrait", "faceState", "videoTrait", "videoState","predictTrait","predictState","convoTrait","convoState","selfState","partnerTrait","partnerState"];
 const traitList = [];
 
 for (let block in data) {
@@ -74,18 +64,20 @@ export const Experiment = ( {subjectID, pairID, socket} ) => {
     const [blockState,setBlockState] = useState(0);
     const [targetState,setTargetState] = useState(0); 
     const [stimState, setStimState] = useState(0);
-    const [traitState,setTraitState] = useState(0);
-    const [rating, setRating] = useState(null);
+    const [traitState,setTraitState] = useState(0); 
+    const [rating, setRating] = useState(0);
+    
 
     // HANDLE STIMULI PRESENTATION
     const [progress, setProgress] = useState(0);
     const [showStim, setShowStim] = useState(false);
-    const [playVideo, setPlayVideo] = useState(false);
-    const [convoTrial, setConvoTrial] = useState(false);
     const [continueStudy, setContinueStudy] = useState(true);
-    const [stimDisplay, setStimDisplay] = useState(0);
+    const [stimDisplay, setStimDisplay] = useState(6000);
     const [ready, setReady] = useState([]);
-    
+    const [trialNum, setTrialNum] = useState(0);
+    const stimTracker = useRef(1)
+
+
     // UPDATING THE DATA OBJECT WITH PARTICIPANT RESPONSES
     const saveData = () => {
       data[blockList[blockState]][targetList[targetState]][stimList[stimState]][traitList[blockState][traitState]] = rating;
@@ -133,100 +125,102 @@ export const Experiment = ( {subjectID, pairID, socket} ) => {
         setTargetState((prev) => prev + 1);
         setStimState(0);
         setTraitState(0);
-        postData();
+        setTrialNum(0);
       }
 
     // HANDLE STIMULI PRESENTATION
-    const handleVideo = () => {
-      if (traitState === 5) {
-        setPlayVideo(true);
-      } else {
-        setPlayVideo(false);
+    const advResponseTrial = () => {
+      if (traitState < 33){
+          saveData();
+          setContinueStudy(true);
+          setTrialNum((prev) => prev + 1);
+          setTraitState((prev) => prev + 1);
+          setProgress((prev) => prev + 1);
+          if (progress === 2){
+            setStimState((prev) => prev + 1);
+            setProgress(0);
+          }
+      } else if (traitState === 33 && blockState !== 1 && ready.length === 2) {
+          advanceBlock();
+      } else if (traitState === 33 && blockState === 1) {
+          setContinueStudy(false);
       }
     }
 
-    const handleConvo = () => {
-      if (traitState === 17) { 
-        setConvoTrial(true);
-      } else {
-        setConvoTrial(false);
-      }
+    const advNonresponseTrial = () => {
+      setContinueStudy(true);
+      setTrialNum((prev) => prev + 1);
+      setStimState((prev) => prev + 1);
     }
 
-    const advanceTrial = () => {
-        if (traitState < 33){
-            saveData();
-            setContinueStudy(true);
-            setTraitState((prev) => prev + 1);
-            setProgress((prev) => prev + 1);
-            handleVideo();
-            handleConvo();
-            if (progress === 2){
-              setStimState((prev) => prev + 1);
-              setProgress(0);
-            }
-        } else if (traitState === 33 && blockState !== 1 && ready.length === 2) {
-            advanceBlock();
-        } else if (traitState === 33 && blockState === 1) {
-            setContinueStudy(false);
-        }
-      }
-
-    // SET PRESENTATION TIMING
+    // HANDLE PRESENTATION TIMING
     // 1-second delay before stimulus display
-
     useEffect(() => {
-        const interval = setTimeout(() => {
-          setShowStim(true);
-        }, 1000);
-    
-        return () => {
-          clearTimeout(interval);
-          setShowStim(false)
-        };
-      }, [traitState]);
+      const interval = setTimeout(() => {
+        setShowStim(true);
+      }, 1000);
+  
+      return () => {
+        clearTimeout(interval);
+        setShowStim(false);
+        stimTracker.current = stimTracker.current + 1;
+      };
+    }, [trialNum]);
 
-    // Conditional timing depending on trial type 
-    useEffect(() => {
-        const timer = setTimeout(() => {
-            if (traitState === 5) {
-                // setStimDisplay(5000) // for testing
-                setStimDisplay(videoDurations[targetList[targetState]]) // target's video duration
-                advanceTrial();
-            } else if (traitState === 17) {
-                setStimDisplay(120000);
-                advanceTrial();
-            } else if (traitState === 33) {
-                setContinueStudy(false)
-            } else {
-                setStimDisplay(5000); //5000 is 5 seconds
-                advanceTrial();
-            }
-        }, stimDisplay);
-        
-        return () => {
-            clearTimeout(timer);
-            setRating(null);
-            setShowStim(false);
-            setReady([]);
-        };
-    }, [traitState]);
+    useEffect( () => { // the way this works is that the 'if' statement condition actually handles the NEXT trial!
+      const trialChange = setTimeout( () => {
+        if (stimList[stimState] === "ITI"){ // this is necessary
+          advNonresponseTrial()
+        } else if (stimState === 2){ // next stim is VidTrial // this is not advancing after the time is done
+          setStimDisplay(videoDurations[targetList[targetState]]) // traitState is undefined? 
+          advResponseTrial();
+        } else if (stimState === 3) { // vidTrial
+          setStimDisplay(6000);
+          advNonresponseTrial();
+        } else if (stimState === 7) { // pre-convoTrial
+          setStimDisplay(20000);
+          advResponseTrial();
+        } else if (stimState === 8) { // convoTrial
+          setStimDisplay(6000);
+          advNonresponseTrial();
+        }
+          else if (stimState === 13) { // Last Trial
+          setContinueStudy(false);
+          advResponseTrial();
+        } else {
+          setStimDisplay(6000)
+          advResponseTrial()
+        }
+      }, stimDisplay);
 
+      return () => {
+        clearTimeout(trialChange)
+      }
+    },[stimTracker.current])
 
     //END TRIAL WHEN BOTH PARTICIPANTS ARE READY
     useEffect(() => {
         if (ready.length === 2) {
             setStimDisplay(5000);
-            advanceTrial()    
+            advNonresponseTrial()    
           }
     }, [ready.length]);
     
     // MONITOR STATE CHANGES
-    console.log("blockstate:",blockState, "targetState:",targetState,"stimState:",stimState,"traitState:",traitState,"video:", playVideo,
-     "convo:",convoTrial,"stimDisplay:",stimDisplay,"continue study:",continueStudy,"ready:",ready,"ready length:",ready.length,"rating:",rating,data)
+    console.log("stimState:",stimList[stimState],"Trait state: ",traitList[traitState])
     
     return (
         <>
+        {stimList[stimState] === "ITI" && showStim && continueStudy &&
+        <>
+          <Grid container justifyContent="center" paddingTop={10}>
+          <Typography style={{color: "#353834", fontSize: 20}} align="center">
+            <p>+</p>
+          </Typography>
+          </Grid>
+        </>
+        }
+
         {stimList[stimState] === "faceTrait" && showStim && continueStudy &&
         <>
           <Grid container justifyContent="center" paddingTop={10}>
@@ -251,7 +245,7 @@ export const Experiment = ( {subjectID, pairID, socket} ) => {
         </>
         }
 
-        {playVideo && showStim && 
+        {stimList[stimState] === "videoTrial" && showStim && continueStudy &&
         <>
           <Grid container justifyContent="center" paddingTop={10}>
           <Typography style={{color: "#353834", fontSize: 20}} align="center">
@@ -262,7 +256,7 @@ export const Experiment = ( {subjectID, pairID, socket} ) => {
         </>
         }
         
-        {stimList[stimState] === "videoTrait" && showStim && playVideo === false && continueStudy &&
+        {stimList[stimState] === "videoTrait" && showStim && continueStudy &&
         <>
           <Grid container justifyContent="center" paddingTop={10}>
           <Typography style={{color: "#353834", fontSize: 20}} align="center">
@@ -274,7 +268,7 @@ export const Experiment = ( {subjectID, pairID, socket} ) => {
         </>
         }
 
-        {stimList[stimState] === "videoState" && showStim && playVideo === false && continueStudy &&
+        {stimList[stimState] === "videoState" && showStim && continueStudy &&
             <>
             <Grid container justifyContent="center" paddingTop={10}>
             <Typography style={{color: "#353834", fontSize: 20}} align="center">
@@ -310,7 +304,7 @@ export const Experiment = ( {subjectID, pairID, socket} ) => {
             </>
         }
 
-        {convoTrial && showStim && 
+        {stimList[stimState] === "convoTrial" && showStim && 
         <>
           <Grid container justifyContent="center" paddingTop={10}>
           <Typography style={{color: "#353834", fontSize: 20}} align="center">
@@ -329,7 +323,7 @@ export const Experiment = ( {subjectID, pairID, socket} ) => {
         </>
         }
 
-        {stimList[stimState] === "convoTrait" && showStim && convoTrial === false && continueStudy &&
+        {stimList[stimState] === "convoTrait" && showStim && continueStudy &&
             <>
             <Grid container justifyContent="center" paddingTop={10}>
             <Typography style={{color: "#353834", fontSize: 20}} align="center">
@@ -341,7 +335,7 @@ export const Experiment = ( {subjectID, pairID, socket} ) => {
             </>
         }
 
-        {stimList[stimState] === "convoState" && showStim && convoTrial === false && continueStudy &&
+        {stimList[stimState] === "convoState" && showStim && continueStudy &&
             <>
             <Grid container justifyContent="center" paddingTop={10}>
             <Typography style={{color: "#353834", fontSize: 20}} align="center">
@@ -388,7 +382,7 @@ export const Experiment = ( {subjectID, pairID, socket} ) => {
             </>
         }
 
-      {traitState === 33 && blockState !== 1 && showStim && continueStudy === false &&
+      {stimList[stimState] === "endTrial" && blockState !== 1 && showStim && continueStudy === false &&
         <>
           <Grid container justifyContent="center" paddingTop={10}>
           <Typography style={{color: "#353834", fontSize: 20}} align="center">
@@ -403,11 +397,12 @@ export const Experiment = ( {subjectID, pairID, socket} ) => {
         </>
       }
 
-      {traitState === 33 && blockState === 1 && showStim && continueStudy === false && 
+      {stimList[stimState] === "endTrial" && blockState === 1 && showStim && continueStudy === false && 
         <>
           <Grid container justifyContent="center">
           <Typography style={{color: "#353834", fontSize: 20}} align="center">
             <p>You have completed the study.</p>
+            <p><em><strong>Do not refresh or exit the browser</strong></em></p>
             <Button style={{color: "#FFFFFF",
                               fontSize: "15px",
                               backgroundColor: "#15b08e"
